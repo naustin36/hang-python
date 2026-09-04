00cd222ac22c703da5361f7bc1091959e56d7a20
naustin36 <naustin3.6@gmail.com>
2026-09-03 22:26:43 -0600

    Added menu functions and ability to change word file

--- main.py
+++ main.py
@@ -1,26 +1,68 @@
 # HangPython
 # Selects random word from list and prompts user for letter guesses to solve the word
 from words import *
-
-word_list_path = "word_list.txt"
+import sys
 
 def main():
-    print("Welcome to HangPython!")
-    word_list: list[str] = []
+    word_list_path: str = "word_list.txt"
+    while True:
+        print("\n\nWelcome to HangPython!")
+        word_list: list[str] = load_words(word_list_path)
+
+        menu_choice = menu()
+
+        if menu_choice == "1":
+            print("Starting game...")
+            input()
+        elif menu_choice == "2":
+            input("coming soon")
+        elif menu_choice == "3":
+            choice = input("Update word file? Y/N >> ")
+            if choice.lower() == "y":
+                new_path = input("Please enter the new file path >> ")
+                try:
+                    validate_file(new_path)
+                    word_list_path = new_path
+                except ValueError as e:
+                    print(e)
+                    input()
+                    continue
+            else:
+                continue
+        elif menu_choice == "4":
+            print("Goodbye!")
+            sys.exit(0)
+        else:
+            print("Please enter the number for the option you wish to select")
+            input()
+            continue
+
+def load_words(word_list_path):
     try:
-        word_list = get_words(word_list_path)
         print(f"Loaded the word library located at {word_list_path}")
+        return get_words(word_list_path)
     except OSError:
         print(f"Error opening file: {word_list_path}")
+        return []
     except Exception as e:
         print(e)
+        return []
 
-    print(f"All words found: {word_list}")
+def validate_file(file_path: str) -> bool | None:
+    if not file_path.endswith(".txt"):
+        raise ValueError("Filetype must be .txt")
+    if len(file_path[0:-4]) == 0:
+        raise ValueError("File name must not be blank")
+    return True
 
-    print(f"Pikcing a random word from {word_list_path}...")
-    word = pick_word(word_list)
-    print(f"{word} is the word!")
 
+def menu():
+    print("Main Menu")
+    print("1. Start Game")
+    print("2. View Stats")
+    print("3. Change Word File")
+    print("4. Quit")
+    return input(">> ")
 
 if __name__ == "__main__":
     main()
